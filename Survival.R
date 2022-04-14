677ea62c3612b5e2ad220a0c8c1d3b773dee489a
MoonligthtFansty <53109144+MoonlightFansty@users.noreply.github.com>
2022-04-14 21:02:45 +0800

    Update Survival.R

--- Survival.R
+++ Survival.R
@@ -1,13 +1,12 @@
 count_survival_filter <- function(fpkm_file='', survival_file=''){
+  library(data.table)
+  
   # 过滤缺失生存信息FPKM矩阵中的样本
-  library(stringr)
-  fpkm <- read.table(fpkm_file, header = T, row.names = 1)
-  survival <- read.table(survival_file, header = T)
-  survival$count_sample <- apply(matrix(survival[,1]), 1, function(str){str_replace_all(str, c('-' = '.'))})
-  survival <- survival[survival$count_sample %in% colnames(fpkm),]
-  fpkm <- fpkm[,colnames(fpkm) %in% survival$count_sample]
-  Ensembl_ID <- row.names(fpkm)
-  fpkm <- cbind(Ensembl_ID, fpkm)
+  fpkm <- fread(fpkm_file, data.table = F)
+  survival <- fread(survival_file, data.table = F)
+  # survival$count_sample <- apply(matrix(survival[,1]), 1, function(str){str_replace_all(str, c('-' = '.'))})
+  survival <- survival[survival$sample %in% colnames(fpkm),]
+  fpkm <- fpkm[,c(TRUE, colnames(fpkm)[-1] %in% survival$sample)]
   survival_filter <- list(fpkm = fpkm, survival = survival)
   
   return(survival_filter)
