bdaeaabe53a47f9d5fb2d1ee7ff93fac3dfddf2a
MoonligthtFansty <53109144+MoonlightFansty@users.noreply.github.com>
2022-04-14 22:58:39 +0800

    Update Survival.R

--- Survival.R
+++ Survival.R
@@ -4,7 +4,6 @@ count_survival_filter <- function(fpkm_file='', survival_file=''){
   # 过滤缺失生存信息FPKM矩阵中的样本
   fpkm <- fread(fpkm_file, data.table = F)
   survival <- fread(survival_file, data.table = F)
-  # survival$count_sample <- apply(matrix(survival[,1]), 1, function(str){str_replace_all(str, c('-' = '.'))})
   survival <- survival[survival$sample %in% colnames(fpkm),]
   fpkm <- fpkm[,c(TRUE, colnames(fpkm)[-1] %in% survival$sample)]
   survival_filter <- list(fpkm = fpkm, survival = survival)
