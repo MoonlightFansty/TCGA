e892ab6763377a3d2d6b97947ed65f10ed2f9fde
MoonligthtFansty <53109144+MoonlightFansty@users.noreply.github.com>
2022-04-21 21:53:55 +0800

    Add files via upload

---
+++ 00矩阵筛选.R
@@ -0,0 +1,66 @@
+# count_survival_filter <- function(fpkm_file='', survival_file=''){
+#   library(data.table)
+#   
+#   # 过滤缺失生存信息FPKM矩阵中的样本
+#   fpkm <- fread(fpkm_file, data.table = F)
+#   survival <- fread(survival_file, data.table = F)
+#   survival <- survival[survival$sample %in% colnames(fpkm),]
+#   fpkm <- fpkm[,c(TRUE, colnames(fpkm)[-1] %in% survival$sample)]
+#   survival_filter <- list(fpkm = fpkm, survival = survival)
+#   
+#   return(survival_filter)
+# }
+# 
+# 
+# survival_filter <- count_survival_filter(fpkm_file='TCGA-BRCA.htseq_fpkm.tsv', survival_file='TCGA-BRCA.survival.tsv')
+# fpkm <- survival_filter$fpkm
+# survival <- survival_filter$survival
+# 
+# fwrite(survival, file='TCGA-BRCA.survival.tsv', sep='\t')
+# fwrite(fpkm, file='TCGA-BRCA.htseq_fpkm.tsv', sep='\t')
+
+
+sample_filter <- function(sample_file='TCGA-sample.xlsx'){
+  # 删除缺失信息的样本
+  sample_id <- readxl::read_excel(sample_file)
+  sample_list <- sample_id[[1]]
+  for (i in length(sample_id[1, ])){
+    if (i != 1){
+      sample_list <- intersect(sample_list, sample_id[[i]])
+    }
+  }
+  
+  return(sample_list)
+}
+
+survival_filter <- function(sample_list, survival_file=''){
+  library(data.table)
+  
+  # 删除缺失信息的样本
+  survival <- fread(survival_file, data.table = F)
+  survival <- survival[survival$sample %in% sample_list,]
+  
+  return(survival)
+}
+
+count_filter <- function(sample_list, fpkm_file=''){
+  library(data.table)
+  
+  # 删除缺失信息的样本
+  fpkm <- fread(fpkm_file, data.table = F)
+  fpkm <- fpkm[,c(TRUE, colnames(fpkm)[-1] %in% sample_list)]
+  
+  return(fpkm)
+}
+
+
+sample_list <- sample_filter(sample_file = 'TCGA-sample.xlsx')
+fpkm <- count_filter(sample_list, fpkm_file='TCGA-BRCA.htseq_fpkm.tsv')
+survival <- survival_filter(sample_list, survival_file='TCGA-BRCA.survival.tsv')
+
+fwrite(fpkm, file='TCGA-BRCA.htseq_fpkm.tsv', sep='\t')
+fwrite(survival, file='TCGA-BRCA.survival.tsv', sep='\t')
+
+
+
+
