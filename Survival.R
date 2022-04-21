c9af0ef5519a6231df4f5e352f67ef475fbc5cab
MoonligthtFansty <53109144+MoonlightFansty@users.noreply.github.com>
2022-04-21 21:54:58 +0800

    Update Survival.R

--- Survival.R
+++ Survival.R
@@ -1,17 +1,3 @@
-count_survival_filter <- function(fpkm_file='', survival_file=''){
-  library(data.table)
-  
-  # 过滤缺失生存信息FPKM矩阵中的样本
-  fpkm <- fread(fpkm_file, data.table = F)
-  survival <- fread(survival_file, data.table = F)
-  survival <- survival[survival$sample %in% colnames(fpkm),]
-  fpkm <- fpkm[,c(TRUE, colnames(fpkm)[-1] %in% survival$sample)]
-  survival_filter <- list(fpkm = fpkm, survival = survival)
-  
-  return(survival_filter)
-}
-
-
 gene_set_xls <- function(DEG_file='', gene_file='', merge_method = 'intersect'){
   # 差异基因 Gene Symbol
   degs <- data.table::fread(DEG_file, data.table = F)
